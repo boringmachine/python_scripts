e95279b7fe8fc31d5fca6e0582d6116f106f7af2
John <john@john-ThinkPad-X60s.(none)>
2013-03-17 22:26:58 +0900

    update

--- src/TextChecker.py
+++ src/TextChecker.py
@@ -1,13 +1,10 @@
 class TextChecker:
         checkString =  ""
-        
-        # param str: string or file
         def read(self,str):
                 self.checkString = ""
                 for line in str: self.checkString += line
                 return len(self.checkString)
-
-        # param str: string or file
+     
         def check(self,str):
                 buf = ""
                 for line in str: buf += line
@@ -23,15 +20,3 @@ class TextChecker:
                 output["count"] = self.check(option["words"])
                 return output
 
-class TextCheckerTester:
-        def input_by_string(self):
-                text = TextChecker()
-                input1 = "this is test words."
-                input2 = "this is test apples"
-                expect = {'count': {'this': 1, 'test': 1, 'is': 2, 'apples': 0}, 'len': 19}
-                actual = text.tc(source=input1,words=input2)
-                assert actual == expect
-                return actual
-
-test = TextCheckerTester()
-test.input_by_string()
